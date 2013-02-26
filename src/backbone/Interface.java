d8fadc760408b760d1e32bb8ea5adcd2b2411cd3
tomas <tomas@elitebook>
2013-02-26 15:43:04 +0100

    Tested and fixed some small bugs in Interface

--- src/backbone/Interface.java
+++ src/backbone/Interface.java
@@ -8,8 +8,13 @@ import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.lang.reflect.Field;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.zip.GZIPInputStream;
 
 import ser2mat.ser2mat;
@@ -26,7 +31,7 @@ public class Interface{
 		Run instance;
 		// Analyse command line arguments, immediately execute some, save rest to Hashtable
 		int NArg = args.length;
-		Hashtable<String, String> argument = new Hashtable<String, String>();
+		Map<String, String> argument = Collections.synchronizedMap(new LinkedHashMap<String, String>());
 		for(int ii=0; ii<NArg; ii+=2) {
 			String arg = args[ii];
 			// Case help file
@@ -135,83 +140,151 @@ public class Interface{
 		}
 	}
 			
-	public static void SetArgument(CModel model, CModel modelRef, Hashtable<String, String> argument) {
-		Enumeration<String> argumentKeys = argument.keys();
-		args:while(argumentKeys.hasMoreElements()) {
-			String keyRaw = argumentKeys.nextElement();
-			String key = keyRaw.contains("[") ? keyRaw.split("\\[")[0] : keyRaw;	// Double escape was necessary
-			String value = argument.get(keyRaw);					// Remove the part at and after "[" if present
+	public static void SetArgument(CModel model, CModel modelRef, Map<String, String> argument) {
+		Iterator<Entry<String, String>> argumentKeys = argument.entrySet().iterator();
+		args:while(argumentKeys.hasNext()) {
+			Entry<String, String> iter = argumentKeys.next(); 
+			String keyRaw = iter.getKey();
+			String key = keyRaw.contains("[") ? keyRaw.split("\\[")[0] : keyRaw;	// Double escape was necessary. Remove the part at and after "[" if present
+			String value = iter.getValue();											
 			for(Field field : CModel.class.getFields()) {
 				if(key.equalsIgnoreCase(field.getName())) {
-					key = field.getName();									// Update key to the correct Capitalisation
+					key = field.getName();						// Update key to the correct Capitalisation
 					try {
 						@SuppressWarnings("rawtypes")
 						Class fieldClass = CModel.class.getField(key).get(model).getClass();
 						// If the field is any kind of array
 						if(field.get(model).getClass().isArray()) {
-							String fieldClassName = fieldClass.getComponentType().getName();
 							// We change only a single index
 							if(keyRaw.contains("[")) {
-								String indexString = keyRaw.split("\\[")[1];
-								indexString = indexString.replace("[","");
-								indexString = indexString.replace("]","");
-								// boolean[]
-								if(fieldClassName.equals("boolean")) {
-									boolean[] modelBool = (boolean[]) field.get(model);
-									boolean[] bool = modelBool.clone();
-									int ii = Integer.parseInt(indexString);
-									bool[ii] = Integer.parseInt(value) == 1 ? true : false;
-									java.util.Arrays.equals(modelBool, bool);
-									if(modelBool[ii] != bool[ii])
-										model.Write(field.getName() + "[" + ii + "] set to " + (bool[ii]?"true":"false"), "");
-									field.set(model, bool);
-									continue args;
-								}
-								// double[]
-								if(fieldClassName.equals("double")) {
-									double[] modelNumber = (double[]) field.get(model);
-									double[] modelRefNumber = (double[]) field.get(modelRef);
-									double[] number = modelNumber.clone();
-									int ii = Integer.parseInt(indexString);
-									// See if we have a relative (e.g. Ka[0] *10) or absolute (e.g. Ka[0] 1e-10) value
-									if(value.startsWith("*")) {	// Relative
-										double multiplier = Double.parseDouble(value.substring(1));		// Cut off *
-										if(number[ii] != modelRefNumber[ii]*multiplier) { 				// If this were true, the operation would already have been done once
-											number[ii] *= multiplier;									// Hasn't been multiplied before, so do it
-											model.Write(field.getName() + "[" + ii + "] set to " + number[ii], "");
-										}
-									} else {
-										number[ii] = Double.parseDouble(value);
-										if(number[ii] != modelRefNumber[ii]) 
-											model.Write(field.getName() + "[" + ii + "] set to " + number[ii], "");
+								String[] keySplit = keyRaw.split("\\[");
+								String iiString = keySplit[1];
+								iiString = iiString.replace("[","");
+								iiString = iiString.replace("]","");
+								// Array of array (matrix)
+								if(keySplit.length>2) {
+									String fieldClassName = fieldClass.getComponentType().getComponentType().getName();
+									String jjString = keySplit[2];
+									jjString = jjString.replace("[","");
+									jjString = jjString.replace("]","");
+									int ii = Integer.parseInt(iiString);
+									int jj = Integer.parseInt(jjString);
+									// boolean[][]					// OPTIMISE: We could make this with generics, virtually the same code is used for all these types
+									if(fieldClassName.equals("boolean")) {
+										boolean[][] modelBool = (boolean[][]) field.get(model);
+										boolean[][] bool= new boolean[modelBool.length][modelBool[0].length];
+										for(int kk=0; kk<modelBool.length; kk++)	bool[kk] = modelBool[kk].clone();		// We need to create a deep copy
+										bool[ii][jj] = Integer.parseInt(value) == 1 ? true : false;
+										java.util.Arrays.equals(modelBool, bool);
+										if(modelBool[ii][jj] != bool[ii][jj])
+											model.Write(field.getName() + "[" + ii + "][" + jj + "] set to " + (bool[ii][jj]?"true":"false"), "");
+										field.set(model, bool);
+										continue args;
 									}
-									field.set(model, number);
-									continue args;
-								}
-								// int[]
-								if(fieldClassName.equals("int")) {
-									int[] modelNumber = (int[]) field.get(model);
-									int[] number = modelNumber.clone();
-									int ii = Integer.parseInt(indexString);
-									number[ii] = Integer.parseInt(value);
-									if(modelNumber[ii] != number[ii])
-										model.Write(field.getName() + "[" + ii + "] set to " + number[ii], "");
-									field.set(model, number);
-									continue args;
-								}
-								// String[]
-								if(fieldClassName.equals("String")) {
-									String[] modelString = (String[]) field.get(model);
-									String[] string = modelString.clone();
-									int ii = Integer.parseInt(indexString);
-									string[ii] = value;
-									if(modelString.equals(string))
-										model.Write(field.getName() + "[" + ii + "] set to " + string[ii], "");
-									field.set(model, string);
-									continue args;
+									// double[][]
+									if(fieldClassName.equals("double")) {
+										double[][] numberRef = (double[][]) field.get(modelRef);
+										double[][] number= new double[numberRef.length][numberRef[0].length];
+										for(int kk=0; kk<numberRef.length; kk++)	number[kk] = numberRef[kk].clone();		// We need to create a deep copy
+										// See if we have a relative (e.g. Ka[0] *10) or absolute (e.g. Ka[0] 1e-10) value
+										if(value.startsWith("*")) {	// Relative
+											double multiplier = Double.parseDouble(value.substring(1));		// Cut off *
+											if(number[ii][jj] != numberRef[ii][jj]*multiplier) { 				// If this were true, the operation would already have been done once
+												number[ii][jj] *= multiplier;									// Hasn't been multiplied before, so do it
+												model.Write(field.getName() + "[" + ii + "][" + jj + "] set to " + number[ii][jj], "");
+											}
+										} else {
+											number[ii][jj] = Double.parseDouble(value);
+											if(number[ii][jj] != numberRef[ii][jj]) 
+												model.Write(field.getName() + "[" + ii + "][" + jj + "] set to " + number[ii][jj], "");
+										}
+										field.set(model, number);
+										continue args;
+									}
+									// int[][]
+									if(fieldClassName.equals("int")) {
+										int[][] modelNumber = (int[][]) field.get(model);
+										int[][] number = new int[modelNumber.length][modelNumber[0].length];
+										for(int kk=0; kk<modelNumber.length; kk++)	number[kk] = modelNumber[kk].clone();		// We need to create a deep copy
+										number[ii][jj] = Integer.parseInt(value);
+										if(modelNumber[ii][jj] != number[ii][jj])
+											model.Write(field.getName() + "[" + ii + "][" + jj + "] set to " + number[ii][jj], "");
+										field.set(model, number);
+										continue args;
+									}
+									// String[][]
+									if(fieldClassName.equals("String")) {
+										String[][] modelString = (String[][]) field.get(model);
+										String[][] string = new String[modelString.length][modelString[0].length];
+										for(int kk=0; kk<modelString.length; kk++)	string[kk] = modelString[kk].clone();		// We need to create a deep copy
+										string[ii][jj] = value;
+										if(modelString[ii][jj].equals(string[ii][jj]))
+											model.Write(field.getName() + "[" + ii + "][" + jj + "] set to " + string[ii][jj], "");
+										field.set(model, string);
+										continue args;
+									}	
+								// Array (vector)									
+								} else {
+									String fieldClassName = fieldClass.getComponentType().getName();
+									// boolean[]
+									if(fieldClassName.equals("boolean")) {
+										boolean[] modelBool = (boolean[]) field.get(model);
+										boolean[] bool = modelBool.clone();
+										int ii = Integer.parseInt(iiString);
+										bool[ii] = Integer.parseInt(value) == 1 ? true : false;
+										java.util.Arrays.equals(modelBool, bool);
+										if(modelBool[ii] != bool[ii])
+											model.Write(field.getName() + "[" + ii + "] set to " + (bool[ii]?"true":"false"), "");
+										field.set(model, bool);
+										continue args;
+									}
+									// double[]
+									if(fieldClassName.equals("double")) {
+										double[] modelNumber = (double[]) field.get(model);
+										double[] modelRefNumber = (double[]) field.get(modelRef);
+										double[] number = modelNumber.clone();
+										int ii = Integer.parseInt(iiString);
+										// See if we have a relative (e.g. Ka[0] *10) or absolute (e.g. Ka[0] 1e-10) value
+										if(value.startsWith("*")) {	// Relative
+											double multiplier = Double.parseDouble(value.substring(1));		// Cut off *
+											if(number[ii] != modelRefNumber[ii]*multiplier) { 				// If this were true, the operation would already have been done once
+												number[ii] *= multiplier;									// Hasn't been multiplied before, so do it
+												model.Write(field.getName() + "[" + ii + "] set to " + number[ii], "");
+											}
+										} else {
+											number[ii] = Double.parseDouble(value);
+											if(number[ii] != modelRefNumber[ii]) 
+												model.Write(field.getName() + "[" + ii + "] set to " + number[ii], "");
+										}
+										field.set(model, number);
+										continue args;
+									}
+									// int[]
+									if(fieldClassName.equals("int")) {
+										int[] modelNumber = (int[]) field.get(model);
+										int[] number = modelNumber.clone();
+										int ii = Integer.parseInt(iiString);
+										number[ii] = Integer.parseInt(value);
+										if(modelNumber[ii] != number[ii])
+											model.Write(field.getName() + "[" + ii + "] set to " + number[ii], "");
+										field.set(model, number);
+										continue args;
+									}
+									// String[]
+									if(fieldClassName.equals("String")) {
+										String[] modelString = (String[]) field.get(model);
+										String[] string = modelString.clone();
+										int ii = Integer.parseInt(iiString);
+										string[ii] = value;
+										if(modelString[ii].equals(string[ii]))
+											model.Write(field.getName() + "[" + ii + "] set to " + string[ii], "");
+										field.set(model, string);
+										continue args;
+									}	
 								}
 							// We make an entirely new array
 							} else {
+								String fieldClassName = fieldClass.getComponentType().getName();
 								String[] splitValue = value.split(",");			// Split at comma
 								for(int ii=0; ii<splitValue.length; ii++) {		// Replace all curly braces
 									splitValue[ii] = splitValue[ii].replace("{","");
